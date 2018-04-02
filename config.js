cbb8bc238a484e014b7ffe81f08d4d5df4e28cee
kaikzero <kaik@protonmail.com>
2018-04-02 23:02:50 +0100

    explorer cn

---
+++ config.js
@@ -0,0 +1,19 @@
+var api = 'http://127.0.0.1:11898';
+var donationAddress = "";
+var blockTargetInterval = 30;
+var coinUnits = 1000000000000;
+var symbol = 'DOSH';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+ "dosh": [
+	["dosh.hashpool.frl", "http://dosh.hashpool.frl:8117"],
+	["mineallcrypto.com/dosh", "http://mineallcrypto.com/dosh:8117"]
+ ]
+};
+
+var networkStat2 = {
+    "dosh": [
+	[""]
+ ]
+};
